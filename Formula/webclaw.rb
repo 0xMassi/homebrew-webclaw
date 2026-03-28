4ba93217be522e94c5905b8b9b21a4ea690e7a5a
Valerio <massimianivalerio1@gmail.com>
2026-03-28 10:45:48 +0100

    fix: downgrade to v0.2.3 (v0.2.4 release was deleted)

--- Formula/webclaw.rb
+++ Formula/webclaw.rb
@@ -2,25 +2,25 @@ class Webclaw < Formula
   desc "The fastest web scraper for AI agents. 67% fewer tokens. Sub-ms extraction."
   homepage "https://webclaw.io"
   license "MIT"
-  version "0.2.4"
+  version "0.2.3"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/0xMassi/webclaw/releases/download/v0.2.4/webclaw-v0.2.4-aarch64-apple-darwin.tar.gz"
-      sha256 "6db6b3e619f0a5cc693072bab42eed816e7bc1dd704aeb96a18e95b0dfffc585"
+      url "https://github.com/0xMassi/webclaw/releases/download/v0.2.3/webclaw-v0.2.3-aarch64-apple-darwin.tar.gz"
+      sha256 "10e3ef395b19b28aea53d204f835b8d226b909439fc220217d1f845bfb39942e"
     else
       url "https://github.com/0xMassi/webclaw/releases/download/v#{version}/webclaw-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "d7d16fb21c0981e5a727fe7b1de4158890f41aa2476dadcbbcdd451b28e02e95"
+      sha256 "d8cb7d6002446c6a63e307d10e2a0e8ca66d78268b1fd665906e0849fd6d5ba5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/0xMassi/webclaw/releases/download/v#{version}/webclaw-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "9228ef6a6288e77127f6ebe005c9c0c12a239582e153ec72057ed2eba409f59d"
+      sha256 "fc373c1bc56dd6644fe059d1d85d31335996ac53876f24087304cdab6e51c0b1"
     else
       url "https://github.com/0xMassi/webclaw/releases/download/v#{version}/webclaw-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "710c8718582c26bc87bdd97890acbd6090f95ec3bf0df74296aa9d9155360e7f"
+      sha256 "1716a4ed3737ef02448aa64fa2967f9a8de70753f707644ea558ef25e898ae35"
     end
   end
 
