ed5e73749ce8a76ce4bd093f43ee2da858efe318
Valerio <88933932+0xMassi@users.noreply.github.com>
2026-03-29 16:45:49 +0200

    webclaw 0.3.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/webclaw.rb
+++ Formula/webclaw.rb
@@ -2,12 +2,12 @@ class Webclaw < Formula
   desc "The fastest web scraper for AI agents. 67% fewer tokens. Sub-ms extraction."
   homepage "https://webclaw.io"
   license "MIT"
-  version "0.2.3"
+  version "0.3.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/0xMassi/webclaw/releases/download/v0.2.3/webclaw-v0.2.3-aarch64-apple-darwin.tar.gz"
-      sha256 "10e3ef395b19b28aea53d204f835b8d226b909439fc220217d1f845bfb39942e"
+      url "https://github.com/0xMassi/webclaw/releases/download/v0.3.0/webclaw-v0.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "af4d1b6c72a1b39eabdc95eff73d126941d88b0811c79cf88c9e60167a613d35"
     else
       url "https://github.com/0xMassi/webclaw/releases/download/v#{version}/webclaw-v#{version}-x86_64-apple-darwin.tar.gz"
       sha256 "d8cb7d6002446c6a63e307d10e2a0e8ca66d78268b1fd665906e0849fd6d5ba5"
