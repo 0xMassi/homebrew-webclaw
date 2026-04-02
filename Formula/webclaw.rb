a563fa0a6075b85b14a7dd91dac5520bfb94ee93
Valerio <massimianivalerio1@gmail.com>
2026-04-02 13:27:17 +0200

    fix: update SHA256 checksums for v0.3.4 + license to AGPL-3.0

--- Formula/webclaw.rb
+++ Formula/webclaw.rb
@@ -1,26 +1,26 @@
 class Webclaw < Formula
   desc "The fastest web scraper for AI agents. 67% fewer tokens. Sub-ms extraction."
   homepage "https://webclaw.io"
-  license "MIT"
+  license "AGPL-3.0"
   version "0.3.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/0xMassi/webclaw/releases/download/v0.3.4/webclaw-v0.3.4-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/0xMassi/webclaw/releases/download/v#{version}/webclaw-v#{version}-aarch64-apple-darwin.tar.gz"
       sha256 "a37124df55f981e59fe06dcf13b80d0cf647d41ee4008026e4fc1ac539e0d525"
     else
       url "https://github.com/0xMassi/webclaw/releases/download/v#{version}/webclaw-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "d8cb7d6002446c6a63e307d10e2a0e8ca66d78268b1fd665906e0849fd6d5ba5"
+      sha256 "9fbadd252e72783c11d2a2dc73dc6ab1260489506de587e7c0a35ee8e235bfbb"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/0xMassi/webclaw/releases/download/v#{version}/webclaw-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "fc373c1bc56dd6644fe059d1d85d31335996ac53876f24087304cdab6e51c0b1"
+      sha256 "dd7eabfda71603b32336d66ea175d047ddc7ef4ddd7130d6277fa79d90d2c5fe"
     else
       url "https://github.com/0xMassi/webclaw/releases/download/v#{version}/webclaw-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "1716a4ed3737ef02448aa64fa2967f9a8de70753f707644ea558ef25e898ae35"
+      sha256 "870ee7bdbea3c1f77bb6dd6f0e80e6a318a629164f61d67d4f1461e6209a04c7"
     end
   end
 
